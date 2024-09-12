5b6f05f4e65f6a06e718ffd2352ca48c6cd3f014
MRAG79 <michal.ragac@outlook.com>
2024-09-13 00:02:45 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import CssGrid from "./components/CssGrid";
+
+export default function Home() {
+  return (
+    <div className="max-w-6xl mx-auto">
+      <CssGrid />
+    </div>
+  );
+}
